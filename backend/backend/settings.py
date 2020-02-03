9fb5f20d12d25b76b2f7697b95f99c224a0dfcc6
Isha Kabra <isha.kabra10@gmail.com>
2020-02-03 16:56:43 -0500

    configure django_heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "..", "build", "static"), ) # update 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
+
+django_heroku.settings(locals())
