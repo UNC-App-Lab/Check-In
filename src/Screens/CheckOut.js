a00bd57fba5079ab7337465daec8f56c2a9039fa
Anna Truelove <anna97@live.unc.edu>
2020-02-12 14:49:29 -0500

    changed axios put in check out

--- src/Screens/CheckOut.js
+++ src/Screens/CheckOut.js
@@ -55,8 +55,18 @@ export default class CheckOut extends React.Component {
             staff: obj.staff
         };
 
-        axios.put(`/api/checkins/${obj.id}/`, item)
-        //axios.put(`https://app-lab-check-in.herokuapp.com/${obj.id}/`, item)
+        //axios.put(`/api/checkins/${obj.id}/`, item)
+
+        axios({ method: 'PUT', url: `/api/checkins/${obj.id}/`, headers: {autorizacion: localStorage.token}, data: { 
+            name: obj.name,
+            PID: obj.PID,
+            date: obj.date,
+            timeIn: obj.timeIn,
+            timeOut: time,
+            reason: obj.reason,
+            checkedIn: false,
+            staff: obj.staff
+          } });
 
         // navigate back to home
         this.props.history.push('');
