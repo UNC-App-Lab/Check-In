f2855d88f26e64002ba80667c842bdc05ec3fe6f
Anna Truelove <anna97@live.unc.edu>
2020-02-14 15:05:03 -0500

    migrated to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,10 +81,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'checkin-db'
     }
 }
 
