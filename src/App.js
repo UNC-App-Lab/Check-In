31ead994e2dbafdd43c1f7c551cdc67a4902f2d5
Anna Truelove <anna97@live.unc.edu>
2020-03-06 13:06:16 -0500

    kpi 1 set up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Screens/Home';
 import CheckIn from './Screens/CheckIn';
 import CheckOut from './Screens/CheckOut';
 import Navigation from './Screens/Navigation';
+import KPI1 from './KPIs/KPI-1';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
              <Route path="/" component={Home} exact/>
              <Route path="/check-in" component={CheckIn}/>
              <Route path="/check-out" component={CheckOut}/>
+             <Route path="/kpi-1" component={KPI1}/>
            </Switch>
         </div> 
       </BrowserRouter>
---
+++ src/KPIs/KPI-1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../App.css';
+
+const kpi1 = () => {
+    return (
+        <div>
+                KPI PAGE
+        </div>
+    );
+}
+
+export default kpi1;
