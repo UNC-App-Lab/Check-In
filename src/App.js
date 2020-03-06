21efa89ec823a5c3063963570e04c603669ef793
Anna Truelove <anna97@live.unc.edu>
2020-03-06 13:16:39 -0500

    added main listing page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Screens/Home';
 import CheckIn from './Screens/CheckIn';
 import CheckOut from './Screens/CheckOut';
 import Navigation from './Screens/Navigation';
+import KPIListings from './KPIs/KPI-Listings';
 import KPI1 from './KPIs/KPI-1';
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
              <Route path="/" component={Home} exact/>
              <Route path="/check-in" component={CheckIn}/>
              <Route path="/check-out" component={CheckOut}/>
+             <Route path="/kpi" component={KPIListings}/>
              <Route path="/kpi-1" component={KPI1}/>
            </Switch>
         </div> 
